7fe901d84a2036a59462dd554d79735bc67c3a7d
diegosunrise <diego@sunriseintegration.com>
2023-10-03 00:44:04 -0500

    fixing final

--- src/components/Final.jsx
+++ src/components/Final.jsx
@@ -1,9 +1,11 @@
 const Final = ({ idioma, disclaimer }) => {
   const { titulo, subtitulo, textoDisclaimer, dhc } = disclaimer;
   const image =
-    idioma === 'ES' ? 'url(/assets/startES.svg)' : 'url(/assets/startPT.svg)';
+    idioma === 'ES'
+      ? 'url(/src/assets/startES.svg)'
+      : 'url(/src/assets/startPT.svg)';
   return (
-    <div className="final" style={{ backgroundImage: image }}>
+    <div className="final">
       <div className="gracias">
         {idioma === 'ES'
           ? 'Gracias por sus Respuestas'
