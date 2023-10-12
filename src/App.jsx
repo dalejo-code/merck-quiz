1e51f7c13e6a9d091a5e173ab108a2c100664097
diegosunrise <diego@sunriseintegration.com>
2023-10-11 20:11:59 -0500

    removing disclaimer file not needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { createContext, useState } from 'react';
 import Data from './Data';
-import Disclaimer from './Disclaimer';
 import Quiz from './Quiz';
 import '@fontsource/sniglet'; // Defaults to weight 400
 const IdiomaContext = createContext(null);
@@ -8,7 +7,7 @@ import { Helmet } from 'react-helmet';
 
 function App() {
   const idioma = import.meta.env.VITE_IDIOMA || 'ES';
-  const zone = import.meta.env.VITE_ZONE || 'ST';
+  const zone = import.meta.env.VITE_ZONA || 'ST';
   const quiz = import.meta.env.VITE_QUIZ || 'DHC';
   return (
     <IdiomaContext.Provider value={idioma}>
